dfe9b144c6b795c66723883af255336a916e5cfd
annieraff <43048489+annieraff@users.noreply.github.com>
2018-12-03 06:46:43 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navbar from "./Navbar";
-import Card from "./Card";
-import Header from "./Header";
+import Navbar from './Navbar';
+import Card from './Card';
+import Header from './Header';
+import Footer from './Footer';
 
 import './index.css';
 
@@ -13,4 +14,5 @@ ReactDOM.render(<div>
     <Header />
     <Navbar />
     <Card />
+    <Footer />            
     </div>, document.getElementById('root'));
